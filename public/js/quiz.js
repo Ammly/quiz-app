854a03319a765cd74f1497d0dda6fd777ca93dab
nicholastanui@gmail.com <ntanui@safaricom.co.ke>
2020-07-24 07:02:14 +0300

    add logic to increment correct score and store wrong answered questions

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -5,31 +5,30 @@ alert("You have selected " + topic + " topic. Good luck");
 
 // Fetch the correct set of questions
 let questions = [{
-        question: "Inside which HTML element do we put the JavaScript??",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+},
+{
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+},
+{
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+}
 ];
-
 
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
@@ -38,7 +37,9 @@ let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
+let correctAnswerCounter = 0;
 let availableQuesions = [];
+let wrongAnswerHolder = [];
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -76,15 +77,13 @@ choices.forEach(choice => {
 
         acceptingAnswers = false;
         const selectedChoice = e.target; // save to db
+        selectedChoice.parentElement.classList.add("selectedAnswer");
         const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply =
-            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-        selectedChoice.parentElement.classList.add(classToApply);
+        selectedAnswer == currentQuestion.answer ? correctAnswerCounter++ : wrongAnswerHolder.push(currentQuestion.question);
 
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
+            selectedChoice.parentElement.classList.remove("selectedAnswer");
             getNewQuestion();
         }, 1000);
     });
